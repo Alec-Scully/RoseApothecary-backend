9e7ff41132bb6b198880bd0996cdb2d786de05c0
Alec Scully <ScullyAlec@gmail.com>
2021-02-23 15:50:30 -0500

    migrations created

---
+++ rose-apothecary-backend/db/migrate/20210223204209_create_cart_item.rb
@@ -0,0 +1,10 @@
+class CreateCartItem < ActiveRecord::Migration[6.1]
+  def change
+    create_table :cart_items do |t|
+      t.integer :cart_id
+      t.integer :item_id
+
+      t.timestamps
+    end
+  end
+end
